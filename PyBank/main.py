50ff00922e7663e72377cceb5d6e386f01d631c9
amarita1 <amarita1@noreply.github.com>
2020-10-17 23:31:03 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@ import csv
 
 totalmonths = []
 Profit_Losses = []
+Counter = 0
 
 print("Financial Analysis")
 print("-----------------------------")
@@ -16,13 +17,21 @@ with open("python-challenge/PyBank/Resources/budget_data.csv", 'r') as input_fil
           Profit_Losses = [int(i) for i in Profit_Losses]
           Changes = [Profit_Losses[i+1] - Profit_Losses[i] for i in range(len(Profit_Losses)-1)]
           Average = sum(Changes)/85
-          
+
      print(f"Total Months : {len(totalmonths)} ")
      print(f"Total: ${sum(Profit_Losses)}")
      print(f"Average Change: ${round(Average, 2)}")
+     
+     Changes_Dc = {totalmonths[i + 1]: Changes[i] for i in range(len(Changes))}         
+     Greatest_Increase = (max(Changes))
+     Greatest_Decrease = (min(Changes))
 
-     print(max(Changes))
-     print(min(Changes))
+     for month, changes in Changes_Dc.items():
+          if changes == Greatest_Increase:
+               print(f"Greatest Increase in Profits: {month} (${Greatest_Increase})")
+          elif changes == Greatest_Decrease:
+               print(f"Greatest Decrease in Profits: {month} (${Greatest_Decrease})")
+
      
 
 
