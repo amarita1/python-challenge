f1504ab8f635fd3f5466d08f13b45a57952cb3d2
amarita1 <amarita1@noreply.github.com>
2020-10-17 20:27:06 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,20 @@
 import csv
 
+Original_List = []
+Unqiue_List = []
+Total_Votes = []
+
 with open("python-challenge/PyPoll/Resources/election_data.csv", 'r') as input_file:
+    csvreader = csv.reader(input_file)
+    header = next(csvreader)
+
+    for row in csvreader:
+        Original_List.append(row[2])
+        Total_Votes.append(row[0])
+    print(Total_Votes)
+
+
+    for x in Original_List:
+        if x not in Unqiue_List:
+            Unqiue_List.append(x)
+        
